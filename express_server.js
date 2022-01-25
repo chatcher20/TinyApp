21bb1f4fb11ba9e914ba48681c8b8708bf1d3e28
Chris Hatcher <hatcherchris20@gmail.com>
2022-01-25 22:15:07 +0000

    updating

--- express_server.js
+++ express_server.js
@@ -13,15 +13,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.listen(PORT, () => {
@@ -46,8 +53,24 @@ app.get("/urls/:shortURL", (req, res) => {      // The : in front of shortURL in
   res.render("urls_show", templateVars);     // //passing templateVars object into urls_show
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
+function generateRandomString() {
 
+  function makeid(length) {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+  console.log(makeid(5));
+};
 
 
 // <!-- This would display the string "http://www.lighthouselabs.ca" -->
